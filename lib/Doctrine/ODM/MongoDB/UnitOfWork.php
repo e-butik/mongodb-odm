b335cfb0a5214860fa44ff148fea1198f149fb7b
Magnus Nordlander <magnus@nordlander.se>
2011-10-11 10:35:35 +0200

    Added patch from JIRA by Kelvin Wood

--- lib/Doctrine/ODM/MongoDB/UnitOfWork.php
+++ lib/Doctrine/ODM/MongoDB/UnitOfWork.php
@@ -612,7 +612,7 @@ class UnitOfWork implements PropertyChangedListener
                             break;
                         }
                     }
-                 }
+                }
             }
         }
     }
@@ -664,7 +664,11 @@ class UnitOfWork implements PropertyChangedListener
      */
     private function computeAssociationChanges($parentDocument, $mapping, $value)
     {
-        if ($value instanceof PersistentCollection && $value->isDirty() && $mapping['isOwningSide']) {
+        $isNewParentDocument = in_array($parentDocument,$this->documentInsertions,true);
+        $class = $this->dm->getClassMetadata(get_class($parentDocument));
+        $topOrExistingDocument = (!$isNewParentDocument || !$class->isEmbeddedDocument);
+
+        if ($value instanceof PersistentCollection && $value->isDirty() && $mapping['isOwningSide'] && $topOrExistingDocument) {
             $owner = $value->getOwner();
             $className = get_class($owner);
             $class = $this->dm->getClassMetadata($className);
@@ -700,10 +704,16 @@ class UnitOfWork implements PropertyChangedListener
                 }
                 $this->persistNew($targetClass, $entry);
                 $this->setParentAssociation($entry, $mapping, $parentDocument, $path);
-                $this->computeChangeSet($targetClass, $entry);
+                if ($topOrExistingDocument)
+                {
+                    $this->computeChangeSet($targetClass, $entry);
+                }
             } else if ($state == self::STATE_MANAGED && $targetClass->isEmbeddedDocument) {
                 $this->setParentAssociation($entry, $mapping, $parentDocument, $path);
-                $this->computeChangeSet($targetClass, $entry);
+                if ($topOrExistingDocument)
+                {
+                    $this->computeChangeSet($targetClass, $entry);
+                }
             } else if ($state == self::STATE_REMOVED) {
                 return new \InvalidArgumentException("Removed document detected during flush: "
                         . self::objToStr($removedDocument).". Remove deleted documents from associations.");
@@ -1551,7 +1561,7 @@ class UnitOfWork implements PropertyChangedListener
                 break;
             case self::STATE_NEW:
                 $this->persistNew($class, $document);
-                break;
+                return;
             case self::STATE_DETACHED:
                 throw new \InvalidArgumentException(
                         "Behavior of persist() for a detached document is not yet defined.");
